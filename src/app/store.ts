b89d2f855fcc2445d31523c30d06a1e1cd64b362
pavodev <pavici@usi.ch>
2021-04-06 10:19:06 +0200

    fix

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,10 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import nodesReducer from './reducers/nodesReducer';
 import questionsReducer from './reducers/questionsReducer';
 import tagsReducer from './reducers/tagsReducer';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     nodes: nodesReducer,
     questions: questionsReducer,
     tags: tagsReducer,
